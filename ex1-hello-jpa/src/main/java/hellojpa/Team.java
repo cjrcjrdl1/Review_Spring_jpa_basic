8a94079b5424d017682b7e172b32901ec4ff7e05
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-08-24 20:47:40 +0900

    양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리

--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -17,6 +17,11 @@ public class Team {
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
+    public void addMember(Member member) {
+        member.setTeam(this);
+        members.add(member);
+    }
+
     public List<Member> getMembers() {
         return members;
     }
@@ -39,5 +44,14 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", members=" + members +
+                '}';
     }
 }
