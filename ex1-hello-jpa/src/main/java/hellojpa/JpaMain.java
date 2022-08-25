c5f4af81be4c38010c246902d1d836bdc75e8ac8
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-08-25 20:19:56 +0900

    상속관계 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,17 +18,19 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("member1");
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과함께사라지다");
+            movie.setPrice(10000);
 
-            em.persist(member);
+            em.persist(movie);
 
-            Team team = new Team();
-            team.setName("teamA");
+            em.flush();
+            em.clear();
 
-            team.getMembers().add(member);
-
-            em.persist(team);
+            Item findMovie = em.find(Item.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
 
             tx.commit();
         } catch (Exception e) {
